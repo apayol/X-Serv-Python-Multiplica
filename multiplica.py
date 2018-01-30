8141dfdc64cc78ced6aa47228353ae72022be335
Adrian Payol Montero <apayol@beta11.aulas.gsyc.urjc.es>
2018-01-30 12:41:42 +0100

    Hechas las tablas

--- multiplica.py
+++ multiplica.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 
-##EJERCICIO MULTIPLICA
+list = range(1,11)
+for num1 in list:
+	print("\nLa tabla del", list)
+	for num2 in list:
+		print(num1, "x", num2, " = ", num1*num2)
+
+
